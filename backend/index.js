c9184210ee35cb141f4586507f63a6859ae8acc4
Kevin Nelms Baxter II <k3vin.baxt3r@gmail.com>
2023-03-26 14:50:34 -0400

    first commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express()
+
+
+const db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "test"
+    })
+
+app.get("/", (req, res) => {
+    res.json("hello this is the backend")
+})
+
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books"
+
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+} )
+
+app.listen(8000, () => {
+    console.log("Connected to backend")
+})
